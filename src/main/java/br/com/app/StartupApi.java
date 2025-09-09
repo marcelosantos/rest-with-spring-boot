e162c2bd7ba7f7bd242eb39b5bee1154120089ca
Marcelo Santos <marcelosantosadm@gmail.com>
2025-09-09 11:33:16 -0300

    Adding feature 01

---
+++ src/main/java/br/com/app/StartupApi.java
@@ -0,0 +1,13 @@
+package br.com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartupApi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartupApi.class, args);
+	}
+
+}
